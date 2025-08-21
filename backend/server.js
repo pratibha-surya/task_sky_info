e6cb091d0a4bdd75529119a39f158a6c3be451cf
pratibha-surya <suryavanshipratibha18@gmail.com>
2025-08-21 19:12:36 +0530

    nkh

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,43 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 
 import categoryRoute from './routes/category.route.js';
 import subcategoryRoute from './routes/subcategory.route.js';
 import productRoute from './routes/product.route.js';
 import { connectDB } from './config.js/db.js';
 
-
 dotenv.config();
 connectDB();
 
 const app = express();
+
+
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
+
+app.use('/api/v1/', categoryRoute);
+app.use('/api/v1/', subcategoryRoute);
+app.use('/api/v1/', productRoute);
 
 
-app.use('/api/v1', categoryRoute);
-app.use('/api/v1', subcategoryRoute);
-app.use('/api/v1', productRoute);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
